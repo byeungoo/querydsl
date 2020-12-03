ec2ee7b464753260b0e7a42aee285e2314284ac0
ByeonGooHoon <dksekfldks65@naver.com>
2020-12-03 23:41:08 +0900

    querydsl querydslPredicateExecutor 예제

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -9,12 +9,14 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -64,6 +66,14 @@ class MemberRepositoryTest {
         
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1");
+    }
+
+    @Test
+    public void querydslPredicateExecutorTest(){
+        Iterable result = memberRepository.findAll(
+                member.age.between(10, 40)
+                        .and(member.username.eq("member1"))
+        );
     }
 
 }
